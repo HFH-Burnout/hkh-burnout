ed2abe6551af5c27773b28247f8734a4dedbaf25
Zubair Mukhi <zubair.mukhi@gmail.com>
2020-04-05 04:34:41 +0000

    Fixing routing (sorry Rose)

--- proj-burnout/src/app/app-routing.module.ts
+++ proj-burnout/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//import { UsersplashComponent} from './usersplash/usersplash.component';
 
 
 const routes: Routes = [
-{ path: 'profile', component: UsersplashComponent}];
+//{ path: 'profile', component: UsersplashComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
