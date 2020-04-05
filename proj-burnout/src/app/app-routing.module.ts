8f8f6892666d0638dcd4ace7b0424a80eaa95c4e
Zubair Mukhi <zubair.mukhi@gmail.com>
2020-04-05 04:12:05 +0000

    Setting up usersplash

--- proj-burnout/src/app/app-routing.module.ts
+++ proj-burnout/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-{ path: 'profile', component: }];
+{ path: 'profile', component: UsersplashComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
