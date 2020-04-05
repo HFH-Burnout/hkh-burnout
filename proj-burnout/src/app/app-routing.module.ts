0c8aabfaec7a988f5ec39f43c9cbcbadc9f51978
Zubair Mukhi <zubair.mukhi@gmail.com>
2020-04-05 00:30:55 -0500

    created a _very very simple_ landing message, still trying to prep token

--- proj-burnout/src/app/app-routing.module.ts
+++ proj-burnout/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-//import { UsersplashComponent} from './usersplash/usersplash.component';
+import { UsersplashComponent } from './usersplash/usersplash.component';
 
 
 const routes: Routes = [
+  { path: 'profile/:token', component: UsersplashComponent }
 //{ path: 'profile', component: UsersplashComponent}
 ];
 
